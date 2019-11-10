b64e42a33bfc6d984a08f57bbf13c25aa96f7ae4
Светлаков Евгений <eugene.svetlakov@gmail.com>
2019-11-10 09:57:25 +0300

    Created Testing Client WebApi

---
+++ Services/WebShopSOA.Clients/Base/BaseClient.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WebShopSOA.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly HttpClient _Client;
+
+        protected readonly string _ServiceAddress;
+
+        protected BaseClient(IConfiguration config, string ServiceAddress)
+        {
+            _ServiceAddress = ServiceAddress;
+
+            _Client = new HttpClient
+            {
+                BaseAddress = new Uri(config["ClientAddress"])
+            };
+
+            var headers = _Client.DefaultRequestHeaders.Accept;
+
+            headers.Clear();
+
+            headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        
+    }
+}
--- Services/WebShopSOA.ServiceHosting/Controllers/ValuesController.cs
+++ Services/WebShopSOA.ServiceHosting/Controllers/ValuesController.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebShopSOA.ServiceHosting.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
