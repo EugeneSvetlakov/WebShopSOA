3c069d53250030acffd3d84a936f1fc90ab7cb72
Светлаков Евгений <eugene.svetlakov@gmail.com>
2019-11-12 21:04:18 +0300

    Editing BaseClient: adding HTTP methods-verbs

--- Services/WebShopSOA.Clients/Base/BaseClient.cs
+++ Services/WebShopSOA.Clients/Base/BaseClient.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebShopSOA.Clients.Base
 {
@@ -28,6 +30,36 @@ namespace WebShopSOA.Clients.Base
 
             headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
-        
+
+        protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancell = default) where T : new()
+        {
+            var response = await _Client.GetAsync(url, Cancell);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>(Cancell);
+            return new T();
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync<T>(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken Cancell = default)
+        {
+            var response = await _Client.PostAsJsonAsync(url, item, Cancell);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync<T>(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancell = default)
+        {
+            var response = await _Client.PutAsJsonAsync(url, item, Cancell);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete<T>(string url) => DeleteAsync<T>(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync<T>(string url, CancellationToken Cancell = default) => 
+            await _Client.DeleteAsync(url, Cancell);
     }
 }
