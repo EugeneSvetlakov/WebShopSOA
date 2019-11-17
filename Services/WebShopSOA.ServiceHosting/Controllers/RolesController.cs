09d9ccab4462b4440f95985a76cea72460e055f0
Светлаков Евгений <eugene.svetlakov@gmail.com>
2019-11-17 12:37:52 +0300

    Prepearing WebApi to Users and Roles

---
+++ Services/WebShopSOA.ServiceHosting/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebShopSOA.DAL;
+
+namespace WebShopSOA.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<IdentityRole> _RoleStore;
+
+        public RolesController(WebShopSOADbContext db)
+        {
+            _RoleStore = new RoleStore<IdentityRole>(db) { AutoSaveChanges = true };
+        }
+    }
+}
---
+++ Services/WebShopSOA.ServiceHosting/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebShopSOA.DAL;
+using WebShopSOA.Domain.Entities;
+
+namespace WebShopSOA.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User> _UserStore;
+
+        public UsersController(WebShopSOADbContext db)
+        {
+            _UserStore = new UserStore<User>(db) { AutoSaveChanges = true };
+        }
+    }
+}
